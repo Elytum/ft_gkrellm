f59e0ae3df4e7aba6c7776b866dc94e0841f2595
Hadrien MOUTAOUKIL <hmoutaou@e2r2p23.42.fr>
2015-06-28 06:58:21 +0200

    f

--- main.cpp
+++ main.cpp
@@ -4,18 +4,21 @@
 #include "OSinfo.class.hpp"
 #include "RAMModule.hpp"
 #include "TimeModule.hpp"
+#include "NameModule.hpp"
 
 int		main( void )
 {
 	Window			win;
-	FooBar*			tooSmall = new FooBar("");
-	OSinfo*			infoOS = new OSinfo();
-	TimeModule*		infoTime = new TimeModule();
+	//FooBar*			tooSmall = new FooBar("");
+	//OSinfo*			infoOS = new OSinfo();
+	//TimeModule*		infoTime = new TimeModule();
+	NameModule*		infoName = new NameModule();
 
 	win.open();
-	win.addModule(tooSmall, 42);
-	win.addModule(infoOS, 42);
-	win.addModule(infoTime, 42);
+	//win.addModule(tooSmall, 42);
+	//win.addModule(infoOS, 42);
+	//win.addModule(infoTime, 42);
+	win.addModule(infoName, 42);
 	while (42) {
 		win.refresh();
 		win.flush();
